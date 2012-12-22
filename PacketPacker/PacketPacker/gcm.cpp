b70f2caf6e1c338884ee8b7f062f982629fafa22
Park June Chul <pjc0247@naver.com>
2012-12-22 18:58:53 +0900

    gcm testbed

--- PacketPacker/PacketPacker/gcm.cpp
+++ PacketPacker/PacketPacker/gcm.cpp
@@ -38,7 +38,7 @@ void GCM_Testbed(){
 	strHeader += "Content-Type:application/x-www-form-urlencoded;charset=UTF-8";
 	strHeader += "\r\n";
 	strHeader += "Authorization:key=";
-	strHeader += "AIzaSyBez13W3YqLkQ6X-cUwmPpBKhODO81GbfU";
+	strHeader += "AIzaSyBnEaX8e6hwBme6tLY6aQQ7znUzF3vzYLA";
 	strHeader += "\r\n\r\n";
 
 	char szHead[8192];
@@ -58,7 +58,7 @@ void GCM_Testbed(){
 		"delay_while_idle=1&"
 		"%s&"
 		"registration_id=%s",
-		"data.test=asdf", Registration ID);
+		"data.test=asdf", "APA91bEWxTv1F_zBmBm6NYOqxiB1jQaoTDlSh1JqKOpGGqgvxK5p6b3kOgYO2ZsZQsQmdiEAHLER0TULIxPOi54H-EQvCHHq91evWSC4DakvKkyVPx5Rnozxw1gAq_3N8aAavlfAsHssyv2SrPqJrogZW-k_YoxHfA");
 
 	HttpSendRequest(hReq , NULL,  0, (LPVOID)szSendData, strlen(szSendData));
 
@@ -69,9 +69,15 @@ void GCM_Testbed(){
 		HTTP_QUERY_STATUS_CODE|HTTP_QUERY_FLAG_NUMBER,
 		&dwStatusCode, &dwStatusCodeLen, NULL);
 
-	/*while (::InternetReadFile (hHandle, szBuffer, 1024, &dwRead) && dwRead > 0)
+	char szBuffer[1024];
+	DWORD dwRead;
+	string strResult;
+
+	while (::InternetReadFile (hReq, szBuffer, 1024, &dwRead) && dwRead > 0)
 	{
 		szBuffer [dwRead] = 0;
 		strResult += szBuffer;
-	}*/
+	}
+
+	printf("%s\n", strResult.c_str());
 }
