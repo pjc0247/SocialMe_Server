2a2d2d4bcd393e25d0aade4514ac2c4ad0e31c72
pjc_main_Asus <pjc0247@naver.com>
2013-01-03 20:30:19 +0900

    FRIEND 수정

--- PacketPacker/PacketPacker/friend.cpp
+++ PacketPacker/PacketPacker/friend.cpp
@@ -10,9 +10,10 @@ bool IsFriended(int db,char *src,char *dst){
 	char qm[256];
 	int len;
 
-	sprintf(qm,	"select * from \"friend\" "
-				"where (\"friend\"=\'%s\' and \"id\"=\'%s\');",
-				src,dst,src,dst); 
+	sprintf(qm,	"select * from friend f1,friend f2 "
+				"where (f1.\"friend\"=\'%s\' and f1.\"id\"=\'%s\') and "
+				"f2.\"friend\"=\'%s\' and f2.\"id\"=\'%s\';",
+				src,dst,dst,src); 
 
 	q = DbPrepare(db,qm);
 	
@@ -33,7 +34,7 @@ CleanUp:
 bool Friend(int db,char *src,char *dst){
 	bool ret = true;
 	int q;
-	char qm[128];
+	char qm[256];
 
 	if(IsFriended(db,src,dst))
 		return false;
@@ -50,7 +51,7 @@ bool Friend(int db,char *src,char *dst){
 	}
 	DbCloseQuery(q);
 
-	sprintf(qm,	"insert into \"friend\" "
+	/*sprintf(qm,	"insert into \"friend\" "
 		"(\"id\",\"friend\") "
 		"values "
 		"(\'%s\',\'%s\');",
@@ -60,15 +61,16 @@ bool Friend(int db,char *src,char *dst){
 	if(ret == false){
 		goto CleanUp;
 	}
-	DbCloseQuery(q);
+	DbCloseQuery(q);*/
 
 	sprintf(qm, "update \"account\" set \"friends\"=\"friends\"+1 where \"id\"=\'%s\'",
-			src, dst);
+		src, dst);
 	q = DbPrepare(db,qm);
 	ret = DbExecute(q);
 	if(ret == false){
 		goto CleanUp;
 	}
+
 
 CleanUp:
 	DbCloseQuery(q);
@@ -78,7 +80,7 @@ CleanUp:
 bool Unfriend(int db,char *src,char *dst){
 	bool ret = true;
 	int q;
-	char qm[128];
+	char qm[256];
 
 	if(!IsFriended(db,src,dst))
 		return false;
@@ -86,16 +88,6 @@ bool Unfriend(int db,char *src,char *dst){
 	sprintf(qm, "delete from friend where "
 				"(\"id\"=\'%s\' and \"friend\"=\'%s\')",
 				src, dst,src,dst);
-	q = DbPrepare(db,qm);
-	ret = DbExecute(q);
-	if(ret == false){
-		goto CleanUp;
-	}
-	DbCloseQuery(q);
-
-	sprintf(qm, "delete from friend where "
-				"(\"id\"=\'%s\' and \"friend\"=\'%s\')",
-				dst,src);
 	q = DbPrepare(db,qm);
 	ret = DbExecute(q);
 	if(ret == false){
@@ -142,6 +134,45 @@ CleanUp:
 }
 
 bool QueryFriendList(int db,char *id,FriendList *list,int min, int max){
+	int ret = true;
+	int q, i;
+	char qm[256];
+	int len = 0;
+	char *sp;
+
+	sprintf(qm,	"select \"friend\" from \"friend\" where \"id\"=\'%s\' and "
+				"rownum between %d and %d;", id, min, max); 
+
+	q = DbPrepare(db,qm);
+	
+	if(!DbExecute(q)){
+		printf("Execute failed\n");
+
+		ret = false;
+		goto CleanUp;
+	}
+	
+	len = max-min+1;
+	list->id = (char*)malloc(sizeof(char) * 16 * len);
+
+	for(i=0;;i++)
+    {
+        if ( !DbCursor(q,1) )
+            break;
+        if(!DbFetch(q))
+			break;
+		
+		len = DbGetString(q, 1, &sp);
+		memcpy(list->id + i*16, sp, len + 1);
+    }
+	list->count = i;
+
+CleanUp:
+	DbCloseQuery(q);
+	return true;
+}
+
+bool QueryWaitingList(int db,char *id,FriendList *list,int min, int max){
 	int ret = true;
 	int q, i;
 	char qm[256];
